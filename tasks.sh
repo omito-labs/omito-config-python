80025498d5e197206e2af71cb787cada431bb4f3
Barath Kumar <barath@mergedup.com>
2022-10-12 02:01:43 +0200

    feat: env plugin

--- tasks.sh
+++ tasks.sh
@@ -4,12 +4,13 @@
 # @author barathvk@gmail.com
 
 # @cmd bump versions
+# @flag --force force the version bump for all packages
 version() {
   dirs=$(ls -d -- */)
   for dir in $dirs; do
     package=$(echo ${dir} | sed 's:/*$::')
     changes=$(git status --porcelain ${package})
-    if [ -z "$changes" ]; then
+    if [ -z "$changes" ] && [ ! ${argc_force} ]; then
       echo "no changes to package ${package}"
     else
       echo "bumping version for package ${package}"
